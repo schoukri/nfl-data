55205198d32a07fb11a4319e1fce1d06f43b6481
Sam Choukri <sam@choukri.net>
2013-01-05 22:52:09 -0800

    parse the description for details of a basic run play

--- bin/parse.pl
+++ bin/parse.pl
@@ -39,6 +39,9 @@ $csv->column_names($csv->getline($fh));
 # gameid,qtr,min,sec,off,def,down,togo,ydline,scorediff,series1stdn,description,scorechange,nextscore,teamwin,offscore,defscore,season
 
 while (my $data = $csv->getline_hr($fh)) {
+  if (my $details = get_details($data)) {
+    $data->{details} = $details;
+  }
   print Dumper($data);
 }
 
@@ -49,6 +52,51 @@ unless ( $csv->eof() ) {
 }
 
 exit 0;
+
+
+sub get_details {
+  my $p = shift;
+
+  # run play
+  $p->{description} =~ m{
+    ^
+    \((?<clock>\d{0,2}:\d{2})\)\s+
+    (?<carrier>\w\.[\w-]+)\s+
+    (?:
+      (?<side>left|right)\s+
+      (?<direction>guard|tackle|end)|(?:up the (?<side>middle))
+    )\s+
+    to\s+(?<team>\w{3})\s+
+    (?<yardline>\d+)\s+
+    for\s+(?<yards>-?\d+)\s+yards?\s+
+    \((?<tacklers>(\s?\w\.[\w-]+)+)\)\.
+    \s*(?<extra>.*)
+    $
+  }x;
+
+  if (scalar (keys %+)) {
+    my %details = (
+                   type => 'run',
+                   clock => $+{clock},
+                   carrier => $+{carrier},
+                   side => $+{side},
+                   direction => $+{direction},
+                   team => $+{team},
+                   yardline => $+{yardline},
+                   yards => $+{yards},
+                   tacklers => [ split(/\s+/, $+{tacklers}) ],
+                   extra => $+{extra},
+                  );
+
+    return \%details;
+
+    my $extra = $+{extra};
+    # parse the extra info here
+    # 'extra' => 'FUMBLES (S.Lee) RECOVERED by DAL-B.Church at DAL 28. B.Church to DAL 34 for 6 yards (C.Snee). Officially a rush for 1 yard.',
+    # 'extra' => 'PENALTY on DAL-D.Bryant Illegal Motion 5 yards enforced at NYG 24 - No Play.',
+  }
+  return undef;
+}
 
 
 sub usage {
